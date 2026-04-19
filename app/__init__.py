cd6af101cf6c28d437ced66e9c487782ac816a34
woozai <wooozai@gmail.com>
2026-04-19 22:39:31 +0300

    update ;ogging

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from app.routes.products import products_bp
 def create_app() -> Flask:
     """Create the Flask app for the products assignment."""
     logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(name)s:%(message)s")
-    logging.getLogger("app.utils.cache").setLevel(logging.INFO)
+    logging.getLogger("app.cache.memory_cache").setLevel(logging.INFO)
 
     app = Flask(__name__)
     app.register_blueprint(products_bp)
